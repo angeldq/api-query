d2c221a1eb7807e8f8268085ff64e0b8c254cb4b
ULASALLE <ULASALLE@SFUAQPWKS005>
2017-10-02 15:57:51 -0500

    primer commit

---
+++ api-query/src/main/java/com/api/query/ApiQueryApplication.java
@@ -0,0 +1,12 @@
+package com.api.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiQueryApplication.class, args);
+	}
+}
